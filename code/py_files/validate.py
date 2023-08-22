2ff86245ee3663d6a40b064f9e700ed066588a84
Phạm Hoàng <hoang1482002@gmail.com>
2023-08-22 13:58:51 +0700

    oke 123

--- code/py_files/validate.py
+++ code/py_files/validate.py
@@ -1,9 +1,6 @@
 import json
-import os
 from copy import deepcopy
-from time import perf_counter
 
-import pandas as pd
 import torch
 from datasets import Dataset
 from peft import PeftModel
@@ -16,13 +13,14 @@ from transformers import (
     pipeline,
 )
 
+MODEL_NAME = "general_chatbot_05_08_23"
 BASE_MODEL_PATH = "bigscience/bloom-3b"
 ADAPTER_PATH = "hoang14/general_chatbot_05_08_23"
 
 ARC_PATH = "/content/arc_validation.json"
-ARC_SIZE = 100
+ARC_SIZE = 100  # max 2568
 MMLU_PATH = "/content/mmlu_validation.json"
-MMLU_SIZE = 100
+MMLU_SIZE = 100  # max 14789
 
 INFERENCE_BATCH_SIZE = 4
 INFERENCE_PROMPT = """Cuộc trò chuyện giữa con người và trợ lý AI.
@@ -41,30 +39,6 @@ GEN_CONFIG = {
 }
 
 
-# prompt templates
-class PromptType:
-    TEXT_SUMMARIZATION = "text_summarization"
-    CONVERSATION_SUMMARIZATION = "conversation_summarization"
-    QA_WITH_CONTEXT = "qa_with_context"
-    LAW_WITH_CONTEXT = "law_with_context"
-
-
-conversation_system_prompt = "Cuộc trò chuyện giữa con người và trợ lý AI.\n"
-
-conversation_summarization_prompt = "Tóm tắt ngắn gọn đoạn hội thoại sau đây:\n{conversation}\nĐoạn hội thoại đã được tóm tắt:\n"
-text_summarization_prompt = (
-    conversation_system_prompt
-    + "[|Con người|] Tóm tắt ngắn gọn đoạn văn bản sau đây:\n{context}\n[|AI|] "
-)
-qa_with_context_prompt = (
-    conversation_system_prompt
-    + "[|Con người|] Trả lời câu hỏi dựa vào đoạn văn bản dưới đây. Chỉ được trả lời dựa trên thông tin nằm trong văn bản được cung cấp.\nCâu hỏi: {question}\nĐoạn văn bản: {context}\n[|AI|] "
-)
-law_with_context_prompt = (
-    conversation_system_prompt
-    + "[|Con người|] Trả lời câu hỏi pháp luật dựa vào những điều luật liên quan dưới đây. Chỉ được trả lời dựa trên thông tin nằm trong điều luật được cung cấp.\nCâu hỏi: {question}\nĐiều luật liên quan:\n{context}\n[|AI|] Câu trả lời tóm tắt: "
-)
-
 # default generation config
 default_gen_config = {
     "temperature": 0.3,
@@ -73,6 +47,69 @@ default_gen_config = {
     "max_new_tokens": 512,
     "repetition_penalty": 1.1,
 }
+
+
+def load_arc(path, size):
+    def mapper(x):
+        choices = [x[t] for t in ["option_a", "option_b", "option_c", "option_d"]]
+        return {
+            "input": INFERENCE_PROMPT.format(
+                question=x["instruction"],
+                choices="\n".join(
+                    f"{i}. {c}" for i, c in zip(["A", "B", "C", "D"], choices)
+                ),
+            ),
+            "ref": x["answer"],
+            "category": x["id"].split("/")[-1].split("_")[0],
+        }
+
+    ds = Dataset.from_json(path)
+    ds = (
+        ds.shuffle()
+        .select(range(min(size, len(ds))))
+        .map(mapper, remove_columns=ds.column_names)
+    )
+    return ds
+
+
+def load_mmlu(path, size):
+    def mapper(x):
+        choices = [x[t] for t in ["option_a", "option_b", "option_c", "option_d"]]
+        return {
+            "input": INFERENCE_PROMPT.format(
+                question=x["instruction"],
+                choices="\n".join(
+                    f"{i}. {c}" for i, c in zip(["A", "B", "C", "D"], choices)
+                ),
+            ),
+            "ref": x["answer"],
+            "category": x["id"].split("/")[0],
+        }
+
+    ds = Dataset.from_json(path)
+    ds = (
+        ds.shuffle()
+        .select(range(min(size, len(ds))))
+        .map(mapper, remove_columns=ds.column_names)
+    )
+    return ds
+
+
+# ds format: {'input': prompt_str, 'ref': str[A, B, C, D], 'category': str}
+eval_datasets = [
+    {
+        "name": "ARC",
+        "path": ARC_PATH,
+        "load_func": load_arc,
+        "size": ARC_SIZE,
+    },
+    {
+        "name": "MMLU",
+        "path": MMLU_PATH,
+        "load_func": load_mmlu,
+        "size": MMLU_SIZE,
+    },
+]
 
 
 class Inferencer:
@@ -84,12 +121,6 @@ class Inferencer:
         tokenizer_max_length=2048,
         human_symbol="[|Con người|]",
     ):
-        self.prompt_templates = {
-            PromptType.TEXT_SUMMARIZATION: text_summarization_prompt,
-            PromptType.CONVERSATION_SUMMARIZATION: conversation_summarization_prompt,
-            PromptType.QA_WITH_CONTEXT: qa_with_context_prompt,
-            PromptType.LAW_WITH_CONTEXT: law_with_context_prompt,
-        }
         self.human_symbol = human_symbol
         self.gen_config = deepcopy(gen_config)
         self.model = None
@@ -106,6 +137,7 @@ class Inferencer:
         self._set_stopping_criteria([self.human_symbol, self.tokenizer.eos_token])
         print("Building generation pipeline...")
         self._build_pipeline()
+        print("Model ready!")
 
     def _load_model_and_tokenizer(
         self, adapter_path, base_model_name_or_path, tokenizer_max_length
@@ -157,140 +189,22 @@ class Inferencer:
             **self.gen_config,
         )
 
-    def _log(
-        self, model_name, generated, time_taken, csv_path_to_save_logs="prompt_log.csv"
-    ):
-        try:
-            df = pd.read_csv(csv_path_to_save_logs)
-            if set(df.columns.tolist()) != set(
-                ["model", "generated", "time_taken", "prompt_time"]
-            ):
-                raise Exception("Columns are not correct")
-        except:
-            os.makedirs(os.path.dirname(csv_path_to_save_logs), exist_ok=True)
-            df = pd.DataFrame(
-                columns=["model", "generated", "time_taken", "prompt_time"]
-            )
-        cur_time = pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
-        df = pd.concat(
-            [
-                df,
-                pd.DataFrame(
-                    [[model_name, generated, time_taken, cur_time]],
-                    columns=["model", "generated", "time_taken", "prompt_time"],
-                ),
-            ]
-        )
-        df.drop_duplicates(subset=["generated"], keep="last").to_csv(
-            csv_path_to_save_logs, index=False
-        )
-
-    def summarize_mode(self, state: bool):
-        if state == True:
-            self._set_stopping_criteria([self.tokenizer.eos_token])
-        else:
-            self._set_stopping_criteria([self.tokenizer.eos_token, self.human_symbol])
-        self._build_pipeline()
-
     def set_gen_config(self, gen_config):
         self.gen_config = gen_config
         self._build_pipeline()
 
-    def format_prompt(self, prompt_type, **kwargs):
-        if prompt_type not in self.prompt_templates:
-            raise ValueError(
-                "Prompt type must be one of the following: {}".format(
-                    self.prompt_templates.keys()
-                )
-            )
-        return self.prompt_templates[prompt_type].format(**kwargs)
-
-    def generate(
-        self,
-        prompt,
-        log=False,
-        csv_path_to_save_logs="./prompt_log.csv",
-        log_model_name="bloom_sft_3b",
-    ):
-        start = perf_counter()
+    def generate(self, prompt):
         text_outputs = [x[0]["generated_text"] for x in self.pipeline(prompt)]
         if isinstance(prompt, str):
             text_output = text_outputs[0]
         else:
             text_output = text_outputs
-        total_time = perf_counter() - start
-        if log:
-            self._log(log_model_name, text_output, total_time, csv_path_to_save_logs)
-            print(f"### Generated in {total_time:.6f} seconds ###\n")
         return text_output
 
 
-def load_arc(path, size):
-    def mapper(x):
-        choices = [x[t] for t in ["option_a", "option_b", "option_c", "option_d"]]
-        return {
-            "input": INFERENCE_PROMPT.format(
-                question=x["instruction"],
-                choices="\n".join(
-                    f"{i}. {c}" for i, c in zip(["A", "B", "C", "D"], choices)
-                ),
-            ),
-            "ref": x["answer"],
-            "category": x["id"].split("/")[-1].split("_")[0],
-        }
-
-    ds = Dataset.from_json(path)
-    ds = (
-        ds.shuffle()
-        .select(range(min(size, len(ds))))
-        .map(mapper, remove_columns=ds.column_names)
-    )
-    return ds
-
-
-def load_mmlu(path, size):
-    def mapper(x):
-        choices = [x[t] for t in ["option_a", "option_b", "option_c", "option_d"]]
-        return {
-            "input": INFERENCE_PROMPT.format(
-                question=x["instruction"],
-                choices="\n".join(
-                    f"{i}. {c}" for i, c in zip(["A", "B", "C", "D"], choices)
-                ),
-            ),
-            "ref": x["answer"],
-            "category": x["id"].split("/")[0],
-        }
-
-    ds = Dataset.from_json(path)
-    ds = (
-        ds.shuffle()
-        .select(range(min(size, len(ds))))
-        .map(mapper, remove_columns=ds.column_names)
-    )
-    return ds
-
-
-# ds format: {'input': prompt_str, 'ref': str[A, B, C, D], 'category': str}
-temp = [
-    {
-        "name": "ARC",
-        "path": ARC_PATH,
-        "load_func": load_arc,
-        "size": ARC_SIZE,
-    },
-    {
-        "name": "MMLU",
-        "path": MMLU_PATH,
-        "load_func": load_mmlu,
-        "size": MMLU_SIZE,
-    },
-]
-
-
 def report(
-    generate_func, ds_path_name_load=temp, bs=INFERENCE_BATCH_SIZE
-):  # generate_func: [input] -> [pred]
+    generate_func, ds_path_name_load=eval_datasets, bs=INFERENCE_BATCH_SIZE
+):  # generate_func: [input_prompts] -> [preds]
     def mapper(x):
         return {
             "input": x["input"],
@@ -300,10 +214,11 @@ def report(
         }
 
     def report_for_category(ds):
-        res = {x: {"ref": [], "pred": []} for x in ds.unique("category")}
+        res = {x: {"ref": [], "pred": [], "input": []} for x in ds.unique("category")}
         for row in ds:
             res[row["category"]]["ref"].append(row["ref"])
             res[row["category"]]["pred"].append(row["pred"])
+            res[row["category"]]["input"].append(row["input"])
         for x in res:
             res[x]["acc"] = sum(
                 1 for i, j in zip(res[x]["ref"], res[x]["pred"]) if i == j
@@ -311,10 +226,10 @@ def report(
         return res
 
     datasets = {
-        x["name"]: x["load_func"](x["path"], x["size"]).map(
-            mapper, batched=True, batch_size=bs
-        )
-        for x in ds_path_name_load
+        x["name"]: x["load_func"](x["path"], x["size"]) for x in ds_path_name_load
+    }
+    datasets = {
+        x: datasets[x].map(mapper, batched=True, batch_size=bs) for x in datasets
     }
     return {x: report_for_category(datasets[x]) for x in datasets}
 
@@ -329,11 +244,9 @@ inferencer.set_gen_config(GEN_CONFIG)
 def generate_func(prompts):
     results = [x[0]["generated_text"] for x in inferencer.pipeline(prompts)]
     indices = [res.find("[|AI|]") for res in results]
-    return [res[i + 7 : i + 8].strip() if i != -1 else "" for i, res in zip(indices, results)]
+    return [res[i + 7 : i + 8] if i != -1 else "" for i, res in zip(indices, results)]
 
 
 result = report(generate_func)
-
-# check if file exist if not create and then write json
-with open(f"result_{ADAPTER_PATH.split('/')[-1]}.json", "w") as f:
+with open(f"result_{MODEL_NAME}.json", "w") as f:
     json.dump(result, f)
